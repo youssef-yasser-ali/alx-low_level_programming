c7531dc8ead96aa82837a41b3ff2d29cf2633e9b
youssef-yasser-ali <yyasser849@gmail.com>
2023-02-16 23:36:58 -0800

    6 -size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("Size of a char: %lu byte(s)", sizeof(char));
+	puts("Size of an int: %lu byte(s)", sizeof(int));
+	puts("Size of a long int: %lu byte(s)", sizeof(long int));
+	puts("Size of a long long int: %lu byte(s)", sizeof(long long int));
+	puts("Size of a float: %lu byte(s)", sizeof(float));
+	return (0);
+}
