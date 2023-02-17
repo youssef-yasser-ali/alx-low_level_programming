26657a2cc2ad99ebc67be915fdd8c77f92decadc
youssef-yasser-ali <yyasser849@gmail.com>
2023-02-17 15:59:52 -0800

    the print digit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+	for (n = 0; n < 10; n++)
+	putchar(n +'0');
+	putchar('\n');
+
+	return (0);
+
+}
