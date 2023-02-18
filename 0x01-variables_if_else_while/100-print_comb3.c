7fe3277e19535d6b30b6d54b6510752a5d5fe080
youssef-yasser-ali <yyasser849@gmail.com>
2023-02-17 17:00:59 -0800

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n, m;
+
+	for (n = '0'; n <= '0' + 8; n++)
+	{
+
+		for (m = '1'; m <= '1' + 8; m++)
+		{
+			putchar(n);
+			putchar(m);
+			if (n == '8' && m == '9')
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+
+}
+
+
