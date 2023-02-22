b70ad56607db5cbd3d532cffb37d109f16243101
youssef-yasser-ali <yyasser849@gmail.com>
2023-02-21 18:01:39 -0800

    push last digit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _abs -a computes abs value
+*
+* @n: int to computer as absoute
+
+* Return: absolute value
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_last_digit - prints last digit
+*
+* @n: number to copute last digit
+*
+* Return: last digit
+*/
+
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+
+	if (n < 0)
+		n = -n;
+
+	_putchar(n + '0');
+	return (n);
+}
