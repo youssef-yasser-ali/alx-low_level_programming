a8e9dabae713563a136a1b0bec81dda60f240e79
youssef-yasser-ali <yyasser849@gmail.com>
2023-02-21 18:14:59 -0800

    editing coment

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,17 +1,22 @@
 #include "main.h"
 
 /**
-* _abs -a computes abs value
+* _abs - compute the absolute value
 *
 * @n: int to computer as absoute
-
+*
 * Return: absolute value
 */
 
 int _abs(int n)
 {
 	if (n < 0)
+	{
 		return (-n);
+	}
 	else
+	{
 		return (n);
+	}
+
 }
